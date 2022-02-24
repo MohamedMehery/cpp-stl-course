d7f7fbea1719b26a83c9984f608a0b172822b030
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2022-02-24 20:42:56 +0200

    super power quiz

--- superpower.cpp
+++ superpower.cpp
@@ -2,15 +2,28 @@
 #include <math.h>
 
 using namespace std;
-
-bool superpower(int z)
+/*
+unsigned int logn(unsigned int n ,unsigned int  r)
 {
+    return (n > r-1)  ? 1+logn((n / r) , r ) : 0;   
+    //this line count the number if ones which means that n is divisable by r
+}*/
+
+/* we can use logarithmic rule mensioned in geekfor geeks */ 
+unsigned int Logn(unsigned int n,
+                  unsigned int r)
+{
+    return log(n) / log(r);
+
+}
+bool superpower(int z)
+{   /* function complixity = O(log(n))  */
     for(int j = z ; j > 1 ; j++)
     {
         for(int i = z-1 ; i > 1 ; i--)
         {
-            if( ceil( logn(j , i)) == floor(logn(j , i)))
-                return 1;;
+            if( ceil( Logn(j , i)) == floor(Logn(j , i))) //if logn return int 
+                return 1;
         }
     }
     return 0;
@@ -18,6 +31,6 @@ bool superpower(int z)
 
 int main()
 {
-
+    cout << "\t" << superpower(9) << endl;
     return 0;
 }
